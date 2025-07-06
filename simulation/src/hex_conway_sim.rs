35430794f787c3ae4a7056ad1a68f78de6b9b528
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-07-06 21:54:22 +0200

    HexConway: Fix wrong geometry orientation. There are still bugs (see TODO)

--- simulation/src/hex_conway_sim.rs
+++ simulation/src/hex_conway_sim.rs
@@ -3,9 +3,15 @@ use raylib::drawing::{RaylibDraw, RaylibDrawHandle};
 use raylib::math::{Vector2};
 use crate::hex_geom::{AxialCoord, HexGeometry, MinMaxAxial};
 
+// TODO: Due intervalli di max min su q e r e basta NON SONO SUFFICIENTI a determinare propriamente
+//  la zona da tenere viva. C'e bisogno di un modo diverso di delimitare una zona valida.
+//  Per fixare il bug dell'overdraw/oversimulating basta sostituire MinMax con un nuovo metodo
+//  per sapere lo spazio delle coordinate assiali che sono valide.
+//  C'e forse il rischio di dover riscrivere tutto con coordinate Offset ma speriamo di no...
+
 pub struct HexConwaySimulation<'a> {
     geo: &'a HexGeometry,
-    pub states: Vec<Vec<u8>>,  // [q(traslato di -min_q)][r]
+    pub states: Vec<Vec<u8>>, // [r][q(translato di -min_q)]. There is a little more data than needed
     birth: Vec<u8>,
     stay: Vec<u8>,
     minmax: MinMaxAxial,
@@ -14,7 +20,8 @@ pub struct HexConwaySimulation<'a> {
 impl<'a> HexConwaySimulation<'a> {
     pub fn new(geo: &'a HexGeometry, birth: &[u8], stay: &[u8]) -> Self {
         let minmax = geo.minmax_axial();
-        let states = vec![vec![0; (minmax.max_r - minmax.min_r) as usize]; (minmax.max_q - minmax.min_q) as usize];
+        assert_eq!(minmax.min_r, 0, "This simulation expect r to start from 0.");
+        let states = vec![vec![0; (minmax.max_q - minmax.min_q) as usize]; (minmax.max_r - minmax.min_r) as usize];
         HexConwaySimulation{
             geo,
             states,
@@ -25,24 +32,18 @@ impl<'a> HexConwaySimulation<'a> {
     }
 
     pub fn get_axial<'b>(states: &'b Vec<Vec<u8>>, a: &AxialCoord, minmax: &MinMaxAxial) -> &'b u8 {
-        states.get((a.q-minmax.min_q) as usize).unwrap().get(a.r as usize).unwrap()
-    }
-
-    pub fn axial_to_statecoords(&self, q: isize, r: isize) -> (usize, usize) {
-        // TODO: do I still need this?
-        let x = q - self.minmax.min_q;
-        let y = r;
-        (x as usize, y as usize)
+        states.get(a.r as usize).unwrap().get((a.q-minmax.min_q) as usize).unwrap()
     }
 
     pub fn step(&mut self) {
         // TODO: HOW THE FUCK DO I ITERATE ON AN HEXAGONAL GRID, FIGLIODELLAMMERDA
         let prev_state = self.states.clone();
         // Coordinate valide da min_q a max_q e da min_r a max_r!
-        for (offq, qline) in self.states.iter_mut().enumerate() {
-            let q = offq as isize + self.minmax.min_q;
-            for (r, state) in qline.iter_mut().enumerate() {
-                let r = r as isize;
+        for (r, rline) in self.states.iter_mut().enumerate() {
+            let r = r as isize;
+            for (offq, state) in rline.iter_mut().enumerate() {
+                let q = offq as isize + self.minmax.min_q;
+                if !self.minmax.validate(AxialCoord{q,r}) {continue}
                 let neighbours = self.geo.neighbours(AxialCoord{q, r});
                 let alives = neighbours.iter().fold(0_u8, |acc, axcord| {
                     if Self::get_axial(&prev_state, axcord, &self.minmax).clone() > 0 { acc+1 } else { acc }
@@ -57,19 +58,17 @@ impl<'a> HexConwaySimulation<'a> {
     }
 
     pub fn draw(&mut self, d: &mut RaylibDrawHandle) {
-        // TODO: not using all the parameters!
-        // W = sqrt(3)*size and H = 2*size
         let w = self.geo.hex_width();
         let h = self.geo.hex_height();
-        for (offq, qline) in self.states.iter().enumerate() {
-            // let q = offq as isize + self.minmax.min_q;
-            // offq from 0 to max_q - min_q, so analogue to ny
-            let row_y_center = 0.5*h + (0.75*h)*offq as f32;
-            let row_offset = if offq % 2 == 0 { 0.5*w } else { w };
-            for (r, state) in qline.iter().enumerate() {
-                let r = r as isize;
-                // r from 0 (min_r) to max_r, so analogue to nx
-                let col_x_center = row_offset + r as f32*w;
+        for (r, rline) in self.states.iter().enumerate() {
+            let row_y_center = 0.5*h + (0.75*h)*r as f32;
+            let row_offset = if r % 2 == 0 { 0.5*w } else { w };
+            for (offq, state) in rline.iter().enumerate() {
+                let q = offq as isize + self.minmax.min_q;
+                println!("q {} r {}", q, r);
+                if !self.minmax.validate(AxialCoord{q,r: r as isize}) {continue}
+                // We keep offq because is from 0 to cols
+                let col_x_center = row_offset + offq as f32*w;
                 let color = if *state > 0 { Color::WHITE } else { Color::BLACK };
                 d.draw_poly(Vector2{x: col_x_center, y: row_y_center}, 6, self.geo.size, 90.0, color);
                 d.draw_poly_lines(Vector2{x: col_x_center, y: row_y_center}, 6, self.geo.size, 90.0, Color::GRAY)
